e92c88efe2b9a92fe3a754e657b43ede72cd71b3
AndreDRI <andredri@proton.me>
2026-05-17 02:00:15 -0300

    (feat): added images and title container

--- src/components/navigation/header_nav.rs
+++ src/components/navigation/header_nav.rs
@@ -5,7 +5,29 @@ use yew::prelude::*;
 pub fn header_nav() -> Html {
     html! {
         <div class="header-container">
-            <h1>{"HeaderNAV"}</h1>
+            <div class="title-container">
+                <div class="title-subcontainer">
+                    <div class="icon-container">
+                        <img src="images/icon.svg" width="64" height="64" />
+                    </div>
+                    <div class="title-text-container">
+                        <h1>{"ALGViewer"}</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="routing-links-container">
+                <div>
+                    <h1>{"Home"}</h1>
+                </div>
+                <div>
+                    <h1>{"Algorithms"}</h1>
+                </div>
+            </div>
+
+            
+            <div class="settings-container">
+                <h1>{"Setting"}</h1>
+            </div>
         </div>
     }
 }
